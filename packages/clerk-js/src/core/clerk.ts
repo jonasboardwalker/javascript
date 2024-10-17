2aaa470798fe1f8a4b95efc65c46213ddb9e5b3e
Alexandros Ntitoras <a.ntitoras@gmail.com>
2024-10-17 18:07:25 +0300

    feat(expo): Add _unstable functions to override the default passkeys functions
    Adding __unstable__createPublicCredentials __unstable__getPublicCredentials __unstable__isWebAuthnSupported
    which are going to be used to override the webAuthnCreateCredential,webAuthnGetCredential and isWebAuthnSupported

--- packages/clerk-js/src/core/clerk.ts
+++ packages/clerk-js/src/core/clerk.ts
@@ -30,6 +30,7 @@ import type {
   ClientResource,
   CreateOrganizationParams,
   CreateOrganizationProps,
+  CredentialReturn,
   DomainOrProxyUrl,
   EnvironmentJSON,
   EnvironmentResource,
@@ -44,6 +45,9 @@ import type {
   OrganizationProfileProps,
   OrganizationResource,
   OrganizationSwitcherProps,
+  PublicKeyCredentialCreationOptionsWithoutExtensions,
+  PublicKeyCredentialWithAuthenticatorAssertionResponse,
+  PublicKeyCredentialWithAuthenticatorAttestationResponse,
   RedirectOptions,
   Resources,
   SDKMetadata,
@@ -189,6 +193,20 @@ export class Clerk implements ClerkInterface {
   #options: ClerkOptions = {};
   #pageLifecycle: ReturnType<typeof createPageLifecycle> | null = null;
   #touchThrottledUntil = 0;
+
+  public __unstable__createPublicCredentials:
+    | ((
+        publicKeyCredential: PublicKeyCredentialCreationOptionsWithoutExtensions,
+      ) => Promise<CredentialReturn<PublicKeyCredentialWithAuthenticatorAttestationResponse>>)
+    | undefined;
+
+  public __unstable__getPublicCredentials:
+    | ((
+        publicKeyCredential: PublicKeyCredentialCreationOptionsWithoutExtensions,
+      ) => Promise<CredentialReturn<PublicKeyCredentialWithAuthenticatorAssertionResponse>>)
+    | undefined;
+
+  public __unstable__isWebAuthnSupported: (() => boolean) | undefined;
 
   get publishableKey(): string {
     return this.#publishableKey;
