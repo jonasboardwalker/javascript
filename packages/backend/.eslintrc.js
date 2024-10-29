9ebcda0a93efab08d9d4a0d60a8a443422e52d1b
Jacek <jacek@clerk.dev>
2024-10-29 16:11:21 -0500

    simplify eslint config

--- packages/backend/.eslintrc.js
+++ packages/backend/.eslintrc.js
@@ -4,14 +4,4 @@ module.exports = {
   rules: {
     'import/no-unresolved': ['error', { ignore: ['^#'] }],
   },
-  overrides: [
-    {
-      files: ['*.test.ts'],
-      rules: {
-        // // TODO: It's an issue specific to QUnit tests
-        // '@typescript-eslint/unbound-method': 'off',
-        '@typescript-eslint/no-unsafe-declaration-merging': 'off',
-      },
-    },
-  ],
 };
